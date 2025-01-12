6a8f59e16231042b7493b00b7c385f1a487748f5
AmithBV0606 <amithrao0606@gmail.com>
2025-01-12 16:17:02 +0530

    Studied about primitive types.

---
+++ TypeScript-Basics/app.ts
@@ -0,0 +1,32 @@
+// Benifits of using TypeScript
+
+// - Define a strict type
+// - Use the same type throughout coding
+
+let variable = "hello";
+
+variable = "hii"; // Here we cannot give number
+
+let age = 18;
+
+// age = "five"; // Throws error
+
+// PRIMITIVE TYPES : 
+
+// Number types : 
+let ageWithType: number = 22;
+
+// ageWithType = "eighteen"; //Throws error
+ageWithType = 18;
+
+// String types : 
+let testString: string = "It's a string!!";
+
+// Boolean types : 
+let testBoolean: boolean = false;
+
+// Multiple types combined : Union types
+let testStringOrNumber: string | number;
+testStringOrNumber = "Amith";
+testStringOrNumber = 30;
+// testStringOrNumber = [];
